02d2268ac2389f14c60aa89f730f19763de0d4b0
Shawn Du <szh.d830@gmail.com>
2025-07-03 17:31:09 +0800

    描述你刚才做了哪些修改，比如：更新按钮样式

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚠️ 仓库名一定要匹配 GitHub Pages 地址路径（注意大小写）
 export default defineConfig({
+  base: '/First-Front-end-Web/',
   plugins: [react()],
 })
